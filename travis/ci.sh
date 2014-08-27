0ed7884f59810ca3af328c32cd60d451730f1cb4
Tjerk Meesters <tjerk.meesters@gmail.com>
2014-08-28 01:25:05 +0800

    Building php-src in a separate directory Link up ext/ustring from already checked out repo

--- travis/ci.sh
+++ travis/ci.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env sh
 
-git clone https://github.com/php/php-src
-cd php-src
-git clone https://github.com/krakjoe/ustring ext/ustring
+set -e
+
+git clone --depth 1 https://github.com/php/php-src ~/php-src
+cd ~/php-src
+ln -s ~/krakjoe/ustring ext/ustring
+
 ./buildconf --force
 ./configure --disable-all --enable-ustring --enable-maintainer-zts
 make -j2 --quiet

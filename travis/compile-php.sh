778850456bb72cd37eee353a1d4c626948727137
Tjerk Meesters <tjerk.meesters@gmail.com>
2014-08-28 01:52:47 +0800

    Fixed compile path of php-src

--- travis/compile-php.sh
+++ travis/compile-php.sh
@@ -5,7 +5,7 @@ set -e
 mkdir -p ~/php
 git clone --depth 1 https://github.com/php/php-src ~/php/src
 
-cd ~/php-src
+cd ~/php/src
 ./buildconf --force
 ./configure --prefix=~/php --disable-all --enable-maintainer-zts
 make -j2 --quiet install

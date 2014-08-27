69eb1e9dca61b4b384ed44b53d40cb60874bf30e
krakjoe <joe.watkins@live.co.uk>
2014-08-27 17:03:44 +0100

    travis is so picky

--- travis/ci.sh
+++ travis/ci.sh
@@ -5,9 +5,10 @@ git clone https://github.com/krakjoe/ustring
 cd ../
 ./buildconf --force
 ./configure --disable-all --enable-ustring --enable-icu --enable-maintainer-zts
-make
+make -j8
 make test TESTS=ext/ustring
 make distclean
 ./configure --disable-all --enable-ustring --enable-icu
+make -j8
 make test TESTS=ext/ustring
 echo "All good things ..."
